d3f99f2508ee067b7655e096f6ce976108fa44d0
Garrett Cox <garrettjcox@gmail.com>
2017-09-10 19:38:44 -0500

    Implement file import

--- src/views/tool/tool.js
+++ src/views/tool/tool.js
@@ -1,10 +1,11 @@
-import { inject } from 'aurelia-framework';
+import { inject, bindable } from 'aurelia-framework';
 import { Documents } from 'services/documents';
 import { tools } from 'services/tools';
 
 @inject(Documents)
 export class Tool {
   canPersist = true;
+  @bindable importFile;
 
   constructor(documents, ) {
     this.documents = documents;
@@ -13,6 +14,26 @@ export class Tool {
   activate({ id }) {
     this.canPersist = tools[id].canPersist;
     this.documents.setTool(id);
+  }
+
+  importFileChanged(files) {
+    let name = files[0].name.split('.');
+    name.pop();
+    name = name.join('.');
+
+    new Promise((resolve) => {
+      const reader = new FileReader();
+      reader.onload = (d) => {
+        resolve(d.target.result);
+      };
+      reader.readAsText(files[0]);
+    })
+      .then(d => {
+        this.documents.create({
+          content: d,
+          title: name
+        });
+      });
   }
 }
 
