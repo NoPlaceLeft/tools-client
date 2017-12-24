1ebbb025f632b8f1eccf4e30b5acc1940b8e73f4
joelcoxokc <joel.cox.dev@gmail.com>
2017-12-24 17:13:43 -0600

    feat(Collapse Sidebar): reposition svg on toggle

--- src/tools/gen-mapper/index.js
+++ src/tools/gen-mapper/index.js
@@ -4,7 +4,6 @@ import { GenMapper } from './gen-mapper';
 export function configure(frameworkConfirguration, config) {
   const configuration = {
     register(tools) {
-      console.log(tools)
       tools.register('gen-mapper', GenMapper)
     }
   }
